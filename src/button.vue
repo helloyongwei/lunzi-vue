d4ee540331fc765e6897b24e5d48e320c2b27740
helloyong <2628578405@qq.com>
2018-09-10 14:38:24 +0800

    extract svg tag to icon.vue

--- src/button.vue
+++ src/button.vue
@@ -1,8 +1,6 @@
 <template>
   <button class="g-button" :class="{[`icon-${iconPosition}`]: true}">
-    <svg v-if="icon" class="icon">
-      <use :xlink:href="`#i-${icon}`"></use>
-    </svg>
+    <g-icon v-if="icon" :name="icon"></g-icon>
     <div class="content">
       <slot></slot>
     </div>
@@ -10,14 +8,13 @@
 </template>
 <script>
   export default {
-//    props: ['icon', 'iconPosition']
     props: {
       icon: {},
       iconPosition: {
         type: String,
         default: 'left',
         validator: function (value) {
-          return value === 'left' || value !== 'right';
+          return value === 'left' || value !== 'right'
         }
 
       }
