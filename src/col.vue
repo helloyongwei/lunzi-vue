e6054891aaa5bfb080f6b35b8ef9a9391ddd6cba
helloyong <2628578405@qq.com>
2018-09-13 17:29:15 +0800

    默认就是phone样式

--- src/col.vue
+++ src/col.vue
@@ -37,12 +37,11 @@ export default {
   },
   computed: {
     colClass(){
-      let {span, offset, phone, ipad, narrowPc, pc, widePc} = this
-      let phoneClass = []
+      let {span, offset, ipad, narrowPc, pc, widePc} = this
       return [
         span && `col-${span}`,
         offset && `offset-${offset}`,
-        ...(phone && [`col-phone-${phone.span}`]),
+
         ...(ipad && [`col-ipad-${ipad.span}`]),
         ...(narrowPc && [`col-narrowPc-${narrowPc.span}`]),
         ...(pc && [`col-pc-${pc.span}`]),
@@ -72,20 +71,7 @@ export default {
         margin-left: ($n / 24) * 100%;
       }
     }
-    @media (max-width: 576px) {
-      $class-prefix: col-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          width: ($n / 24) * 100%;
-        }
-      }
-      $class-prefix: offset-phone-;
-      @for $n from 1 through 24 {
-        &.#{$class-prefix}#{$n} {
-          margin-left: ($n / 24) * 100%;
-        }
-      }
-    }
+
     @media (min-width: 577px) and (max-width: 768px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
