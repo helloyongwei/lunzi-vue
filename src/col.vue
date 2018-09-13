5eec2f5c1399650a03cff500216946ee473c02da
helloyong <2628578405@qq.com>
2018-09-13 20:22:05 +0800

    完成 col 的测试

--- src/col.vue
+++ src/col.vue
@@ -1,75 +1,68 @@
+
 <template>
   <div class="col" :class="colClass" :style="colStyle">
     <slot></slot>
   </div>
 </template>
 <script>
-let validator = (value) => {
-  let keys = Object.keys(value)
-  let valid = true
-  keys.forEach(key => {
-    if (!['span', 'offset'].includes(key)) {
-      valid = false
-    }
-  })
-  return valid
-}
-export default {
-  name: 'LunziCol',
-  props: {
-    span: {
-      type: [Number, String]
-    },
-    offset: {
-      type: [Number, String]
-    },
-
-    ipad: {type: Object, validator},
-    narrowPc: {type: Object, validator},
-    pc: {type: Object, validator},
-    widePc: {type: Object, validator},
-
-  },
-  data() {
-    return {
-      gutter: 0
-    }
-  },
-  methods: {
-    createClasses (obj, str = '') {
-      if (!obj) {
-        return []
+  let validator = (value) => {
+    let keys = Object.keys(value)
+    let valid = true
+    keys.forEach(key => {
+      if (!['span', 'offset'].includes(key)) {
+        valid = false
       }
-      let array = []
-      if (obj.span) {
-        array.push(`col-${str}${obj.span}`)
-      }
-      if (obj.offset) {
-      array.push(`offset-${str}${obj.offset}`)
-      }
-    }
-  },
-  computed: {
-    colClass(){
-      let {span, offset, ipad, narrowPc, pc, widePc} = this
-      let createClasses = this.createClasses
-      return [
-        createClasses({span, offset}),
-        createClasses(ipad, 'ipad-'),
-        createClasses(narrowPc, 'narrow-pc'),
-        createClasses(pc, 'pc'),
-        createClasses(widePc, 'wide-pc-'),
-
-      ]
+    })
+    return valid
+  }
+  export default {
+    name: 'GuluCol',
+    props: {
+      span: {
+        type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      },
+      ipad: {type: Object, validator,},
+      narrowPc: {type: Object, validator,},
+      pc: {type: Object, validator,},
+      widePc: {type: Object, validator,}
     },
-    colStyle(){
+    data () {
       return {
-        paddingLeft:this.gutter/2+'px',
-        paddingRight:this.gutter/2+'px'
+        gutter: 0,
+      }
+    },
+    methods: {
+      createClasses (obj, str = '') {
+        if (!obj) {return []}
+        let array = []
+        if (obj.span) { array.push(`col-${str}${obj.span}`) }
+        if (obj.offset) { array.push(`offset-${str}${obj.offset}`) }
+        return array
+      }
+    },
+    computed: {
+      colClass () {
+        let {span, offset, ipad, narrowPc, pc, widePc} = this
+        let createClasses = this.createClasses
+        return [
+          ...createClasses({span, offset}),
+          ...createClasses(ipad, 'ipad-'),
+          ...createClasses(narrowPc, 'narrow-pc-'),
+          ...createClasses(pc, 'pc-'),
+          ...createClasses(widePc, 'wide-pc-'),
+        ]
+      },
+      colStyle () {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px',
+        }
       }
     }
   }
-}
 </script>
 <style scoped lang="scss">
   .col {
@@ -85,8 +78,7 @@ export default {
         margin-left: ($n / 24) * 100%;
       }
     }
-
-    @media (min-width: 577px)  {
+    @media (min-width: 577px) {
       $class-prefix: col-ipad-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -100,7 +92,7 @@ export default {
         }
       }
     }
-    @media (min-width: 769px){
+    @media (min-width: 769px){ // 770
       $class-prefix: col-narrow-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
@@ -114,7 +106,7 @@ export default {
         }
       }
     }
-    @media (min-width: 993px){
+    @media (min-width: 993px) {
       $class-prefix: col-pc-;
       @for $n from 1 through 24 {
         &.#{$class-prefix}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -18,8 +18,6 @@
       }
     },
     mounted () {
-      console.log('row mounted')
-      console.log(this.$children)
       this.$children.forEach((vm) => {
         vm.gutter = this.gutter
       })
