62f5e497a96ed0421657c08a4dd9a600203470b6
helloyong <2628578405@qq.com>
2018-09-16 11:30:17 +0800

    重构tabs.vue

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="xxx">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -19,22 +19,36 @@
       }
     },
     methods: {
-      xxx(){
-        this.visible = !this.visible
-        if (this.visible === true) {
-          this.$nextTick(()=>{
-            document.body.appendChild(this.$refs.contentWrapper)
-            let {width, height,top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-            this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-            let eventHandler = ()=>{
-              this.visible = false
-
-              document.removeEventListener('click', eventHandler)
-            }
-          })
-        } else {
-          console.log('vm 隐藏 popover')
+      positionContent() {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument(e) {
+        if (this.$refs.popover && (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+          return
+        }
+        this.close()
+      },
+      open() {
+        this.visible = true
+        this.$nextTick(() => {
+          this.positionContent()
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close() {
+        this.visible = false
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick(event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
       }
     }
@@ -47,6 +61,7 @@
     vertical-lign: top;
     position: relative;
   }
+
   .content-wrapper {
     position: absolute;
     transform: translateY(-100%);
